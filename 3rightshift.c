a81d24cb9ac808f747f5b0e8c30b09dfadbdc560
mdrehanalam-2008 <mdrehanalam280408@gmail.com>
2026-08-29 15:16:36 +0530

    Uploading 2rightshift
    A C program to shift right 3 times a number.

---
+++ 3rightshift.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the number : ");
+	scanf("%d",&a);
+	b=a>>3;
+	printf("3 times shift right of the given number : %d\n",b);
+	getch();
+	return 0;
+}
