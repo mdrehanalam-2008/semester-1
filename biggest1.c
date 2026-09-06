e8ee4d57a43124f5f44d795219c142d9daf4baa2
mdrehanalam-2008 <mdrehanalam280408@gmail.com>
2026-09-06 15:00:50 +0530

    Uploading biggest1
    A program in C to compare two numbers and find the bigger one using if statement only.

---
+++ biggest1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("Enter 1st number : ");
+	scanf("%d",&a);
+	printf("Enter 2nd number : ");
+        scanf("%d",&b);
+	if(a>b)
+	printf("Bigger number : %d\n",a);
+	if(a<b)
+	printf("Bigger number : %d\n",b);
+	getch();
+	return 0;
+}
